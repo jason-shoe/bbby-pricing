6f479acdd8d923282825d388ad963ba6caeab9d5
Jason Xu <gwangsenshoe@gmail.com>
2020-07-16 13:21:14 -0400

    Updates

---
+++ precache-manifest.d602eed88d5a5c694075695056eb5b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f84703cf516d23bb7403261f904d32e",
+    "url": "/bbby-pricing/index.html"
+  },
+  {
+    "revision": "88338398170af4214ce3",
+    "url": "/bbby-pricing/static/css/main.22df9116.chunk.css"
+  },
+  {
+    "revision": "c6e942acbca10aed0ac8",
+    "url": "/bbby-pricing/static/js/2.ffdaada1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bbby-pricing/static/js/2.ffdaada1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88338398170af4214ce3",
+    "url": "/bbby-pricing/static/js/main.8b7a946d.chunk.js"
+  },
+  {
+    "revision": "d761e8919a49031026eb",
+    "url": "/bbby-pricing/static/js/runtime-main.f73e2074.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bbby-pricing/static/media/logo.5d5d9eef.svg"
+  }
+]);
